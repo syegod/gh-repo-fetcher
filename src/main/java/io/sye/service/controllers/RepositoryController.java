edd0da07ba74c925e113b3cbd329f87041c88a72
syegod <ovisochin362@gmail.com>
2025-07-27 19:43:51 +0200

    WIP

--- src/main/java/io/sye/service/controllers/RepositoryController.java
+++ src/main/java/io/sye/service/controllers/RepositoryController.java
@@ -1,10 +1,8 @@
 package io.sye.service.controllers;
 
 import io.sye.api.ListRepositoriesResponse;
-import io.sye.service.ServiceException;
 import io.sye.service.services.RepositoryService;
 import java.util.concurrent.CompletableFuture;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
